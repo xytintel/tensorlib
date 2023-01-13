5e226e2cf703fd160ff47a5103dd7ad7e02c887c
Yutao Xu <yutao.xu@intel.com>
2023-01-13 16:42:14 +0800

    Update string.h

--- string.h
+++ string.h
@@ -1,166 +1,54 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
-
-#include "exception.h"
-#include "array.h"
-#include "data_ptr.h"
-#include "intrusive_ptr.h"
-#include "scalar_type.h"
-#include "launcher.h"
+#include <string>
+#include <sstream>
 
 namespace tensorlib {
+namespace utils {
 
-namespace memory {
-
-void delete_impl(void *ctx) {
-    GpuLauncher::GetInstance()->free(ctx);
+inline std::ostream &_str(std::ostream &ss) {
+    return ss;
 }
 
-class TensorStorage : public intrusive_ptr_target {
-protected:
-    DataPtr ptr_;
-    size_t size_;
-    int device_;
+template <typename T>
+inline std::ostream &_str(std::ostream &ss, const T &t) {
+    ss << t;
+    return ss;
+}
 
-public:
-    TensorStorage() :
-        ptr_(), size_(0), device_(0) {
-    }
-    TensorStorage(size_t size, int device) :
-        size_(size), device_(device) {
-        auto l = GpuLauncher::GetInstance();
-        if (l->device() != device_) l->set_device(device_);
-        auto raw_ptr = (void *)l->malloc<char>(size_);
-        DataPtr ptr(raw_ptr, raw_ptr, delete_impl);
-        ptr_ = std::move(ptr);
-    }
-    ~TensorStorage() {
-        auto l = GpuLauncher::GetInstance();
-        if (l->device() != device_) l->set_device(device_);
-        ptr_.clear();
-    }
-    int device() const {
-        return device_;
-    }
-    void *data_ptr() const {
-        return ptr_.get();
-    }
-    template <typename T>
-    T *data_ptr() const {
-        return reinterpret_cast<T *>(ptr_.get());
-    }
-    bool defined() const {
-        return static_cast<bool>(ptr_);
-    }
-    size_t size() const {
-        return size_;
+template <typename T, typename... Args>
+inline std::ostream &_str(std::ostream &ss, const T &t, const Args &... args) {
+    return _str(_str(ss, t), args...);
+}
+
+template <typename... Args>
+struct _str_wrapper final {
+    static std::string call(const Args &... args) {
+        std::ostringstream ss;
+        _str(ss, args...);
+        return ss.str();
     }
 };
 
-} // namespace memory
-
-#define MAX_TENSOR_DIM 12
-
-using namespace memory;
-
-class Tensor;
-Tensor empty(std::vector<uint64_t> shape, ScalarType dtype, int device = 0);
-Tensor zeros(std::vector<uint64_t> shape, ScalarType dtype, int device = 0);
-
-class Tensor {
-    using dim_array_t = memory::array<uint64_t, MAX_TENSOR_DIM>;
-    int dim_;
-    dim_array_t shape_;
-    dim_array_t stride_;
-    ScalarType dtype_;
-    uint64_t numel_;
-
-    intrusive_ptr<memory::TensorStorage> storage_;
-
-    void new_storage_(int device) {
-        size_t bytes = shape_[0] * stride_[0] * element_size(dtype_);
-        auto ptr = new memory::TensorStorage(bytes, device);
-        storage_.unsafe_set_ptr(ptr);
-    }
-    friend Tensor empty(std::vector<uint64_t> shape, ScalarType dtype, int device);
-    friend Tensor zeros(std::vector<uint64_t> shape, ScalarType dtype, int device);
-
-public:
-    Tensor(std::vector<uint64_t> &shape, ScalarType dtype) :
-        dtype_(dtype) {
-        CHECK_FAIL(shape.size() <= MAX_TENSOR_DIM);
-        dim_ = shape.size();
-        numel_ = 1;
-        for (int i = dim_ - 1; i >= 0; i--) {
-            stride_[i] = numel_;
-            numel_ *= shape[i];
-            shape_[i] = shape[i];
-        }
-    }
-    Tensor(const Tensor &other) :
-        dim_(other.dim_), shape_(other.shape_), stride_(other.stride_),
-        dtype_(other.dtype_), numel_(other.numel_),
-        storage_(other.storage_) {
-    }
-    Tensor &operator=(const Tensor &other) {
-        dim_ = other.dim_;
-        shape_ = other.shape_;
-        stride_ = other.stride_;
-        dtype_ = other.dtype_;
-        numel_ = other.numel_;
-        storage_ = other.storage_;
-        return *this;
-    }
-    Tensor(Tensor &&other) = default;
-    Tensor &operator=(Tensor &&other) = default;
-    Tensor() :
-        storage_() {
-    }
-    bool defined() const {
-        return storage_.get() != nullptr;
-    }
-    uint64_t numel() const {
-        return numel_;
-    }
-    int dim() const {
-        return dim_;
-    }
-    int device() const {
-        return storage_.get()->device();
-    }
-    uint64_t shape(int d) const {
-        return shape_[d];
-    }
-    uint64_t stride(int d) const {
-        return stride_[d];
-    }
-    void *data_ptr() const {
-        return storage_.get()->data_ptr();
-    }
-    size_t storage_bytes() const {
-        return storage_.get()->size();
-    }
-    size_t storage_ref_count() const {
-        return storage_.ref_count();
-    }
-    intrusive_ptr<memory::TensorStorage> storage() const {
-        return storage_;
+template <>
+struct _str_wrapper<std::string> final {
+    static const std::string &call(const std::string &str) {
+        return str;
     }
 };
 
-Tensor empty(std::vector<uint64_t> shape, ScalarType dtype, int device) {
-    Tensor output(shape, dtype);
-    output.new_storage_(device);
-    return output;
+template <>
+struct _str_wrapper<const char *> final {
+    static const char *call(const char *str) {
+        return str;
+    }
+};
+
+template <typename... Args>
+inline decltype(auto) str(const Args &... args) {
+    return _str_wrapper<Args...>::call(args...);
 }
 
-Tensor zeros(std::vector<uint64_t> shape, ScalarType dtype, int device) {
-    Tensor output(shape, dtype);
-    output.new_storage_(device);
-    GpuLauncher::GetInstance()->memset(output.data_ptr(), 0, output.storage_bytes());
-    return output;
 }
-
-} // namespace tensorlib
+}; // namespace tensorlib::utils
